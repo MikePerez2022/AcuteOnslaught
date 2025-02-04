3823b1af4e6f3ac3002a14f4cbfd2d89a7d6b528
DPartney <129333259+DPartney@users.noreply.github.com>
2025-02-04 11:10:02 -0700

    Added Comments to ScoreManager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public float currentScore;
-    public TMP_Text currentScoreText;
+    public TMP_Text currentScoreText; // Can be removed if we'll only display the final highscore
     public TMP_Text finalScoreText;
     public TMP_Text highestScoreText;
 
+    // Call whenever we'll calculate/update score: Wave Ended, Time Intervals, etc.
     public void ChangeScore(int points)
     {
         currentScore += points;
         currentScoreText.text = currentScore.ToString();
     }
 
+    // Call at end of the game
     public void UpdateHighScore()
     {
         if (PlayerPrefs.HasKey("HighestScore"))
