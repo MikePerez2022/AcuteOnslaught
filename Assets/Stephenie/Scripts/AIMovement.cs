08d4cd00262f0d7020709b3e957411403d2fe0a8
StephenieDugan <stepheniedugan617@gmail.com>
2025-02-05 10:35:39 -0700

    Added rotation to AImovement
    enemy now looks at player and moves toward them

--- Assets/Stephenie/Scripts/AIMovement.cs
+++ Assets/Stephenie/Scripts/AIMovement.cs
@@ -17,8 +17,12 @@ public class AIMovement : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.forward;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // angle to turn towards player
     
+        //Update position and rotation to move to and look at player
         transform.position = Vector2.MoveTowards(this.transform.position,player.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle); 
     }
     
 }
