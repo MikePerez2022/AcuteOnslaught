d6f36224d9329b71272d5f1ce93bd125344e9bf1
DPartney <129333259+DPartney@users.noreply.github.com>
2025-02-13 09:49:42 -0700

    Updated Health script for combo and score

--- Assets/Stephenie/Scripts/AIScoring.cs
+++ Assets/Stephenie/Scripts/AIScoring.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class AIScoring : MonoBehaviour
 {
-    [SerializeField] int score;
+    [SerializeField] public int score;
     [SerializeField] public int cost;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
