d74417d140eb3fe272f7ab8a74dcd5acf04f176d
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-02-21 13:34:18 -0700

    Audio & UI fixes

--- Assets/Mike/Scripts/UI/NavUI.cs
+++ Assets/Mike/Scripts/UI/NavUI.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class NavUI : MonoBehaviour
 {
@@ -10,17 +11,34 @@ public class NavUI : MonoBehaviour
     [SerializeField] GameObject StartScreen;
     [SerializeField] GameObject GameScreen;
     [SerializeField] GameObject DeathScreen;
+    [SerializeField] GameObject PauseScreen;
     [SerializeField] GameObject SettingsScreen;
 
     [Header("Game:")]
     [SerializeField] GameObject Player;
+    [SerializeField] GameObject A_Music;
+    [SerializeField] GameObject M_Music;
+    [SerializeField] GameObject D_Music;
     [SerializeField] GameObject Spawner;
     [SerializeField] bool test;
+
+    [Header("Audio:")]
+    [SerializeField] List<AudioSource> AudioList;
+
+    private bool isPaused = false;
+    private bool isDead = false;
 
 
     void Update()
     {
-        if (Player.IsDestroyed()) DeathScreen.SetActive(true);
+        if (Player.IsDestroyed() && !isDead)
+        {
+            DeathScreen.SetActive(true);
+            A_Music.SetActive(false);
+            D_Music.SetActive(true);
+            PlaySound(2);
+            isDead = true;
+        }
         if (test) DeathScreen.SetActive(true);
     }
 
@@ -30,40 +48,57 @@ public class NavUI : MonoBehaviour
         GameScreen.SetActive(true);
         Player.SetActive(true);
         Spawner.SetActive(true);
-        print("Start Btn worked");
+        A_Music.SetActive(true);
+        M_Music.SetActive(false);
+        PlaySound(0);
     }
 
     public void OnSettingsBtnPressed()
     {
         SettingsScreen.SetActive(true);
-        print("Settings Btn worked");
+        PlaySound(0);
     }
 
     public void OnSaveBtnPressed()
     {
-        print("Save Btn worked");
+        SettingsScreen.GetComponent<SettingsUI>().OnSave();
+        PlaySound(1);
     }
 
     public void OnBackBtnPressed()
     {
         SettingsScreen.SetActive(false);
-        print("Back Btn worked");
+        PlaySound(0);
     }
 
     public void OnRestartBtnPressed()
     {
-        SceneManager.LoadScene("CombinedScene");
-        print("Back Btn worked");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        PlaySound(0);
+    }
+
+    public void OnPauseBtnPressed()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = (isPaused) ? 0.0f : 1.0f;
+        PauseScreen.SetActive(isPaused);
+
+        PlaySound(0);
     }
 
     public void OnQuitBtnPressed()
     {
-        print("Quit Btn worked");
+        PlaySound(0);
 
         #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
         #else
                     Application.Quit();
         #endif
+    }
+
+    private void PlaySound(int ind)
+    {
+        AudioList[ind].Play();
     }
 }
